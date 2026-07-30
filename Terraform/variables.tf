cced9cccbb18e282e9f5576f3f2d1d6436ca7a0f
einfach7400 <yajiv7400@gmail.com>
2026-07-30 21:35:01 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,5 +1,25 @@
 variable "aws_region" {
-  description = "AWS region to deploy the EKS cluster"
-  type        = string
-  default     = "us-east-1"
+   description = " Name of the aws region "
+   type = string
+   value = "eu-north-1"
+}
+variable "environment" {
+   description = " environment (dev, staging, production "
+   type = string
+   value = "dev"
+}
+variable "cluster_name" {
+   description = " EKS cluster name "
+   type = string
+   value = "jerney-eks"
+}
+variable "cluster_version" {
+   description = " EKS cluster kubernetes version  "
+   type = string
+   value = "1.3"
+}
+variable "vpc_cidr" {
+   description = "  vpc cidr valid range    "
+   type = string
+   value = "10.0.0.0/16"
 }
